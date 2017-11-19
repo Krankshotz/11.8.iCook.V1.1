0a6093587347db1cf48639e5884122b5c128e083
oscaraesparza <oscaraesparza@gmail.com>
2017-11-19 11:56:37 -0800

    Changed icon + other changes to the preferences

--- app/src/main/java/com/example/jordan/icook/PreferencesActivity.java
+++ app/src/main/java/com/example/jordan/icook/PreferencesActivity.java
@@ -1,7 +1,10 @@
 package com.example.jordan.icook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PreferencesActivity extends AppCompatActivity {
 
