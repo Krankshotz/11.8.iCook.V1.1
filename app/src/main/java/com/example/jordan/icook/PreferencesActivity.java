533d0f4f67b70658be281cb8a7af96ec4d5b02ee
Krankshotz <jordyg32@live.com>
2017-11-26 14:45:07 -0800

    Preference layout changed. Now able to swipe left and right for shortcuts.

--- app/src/main/java/com/example/jordan/icook/PreferencesActivity.java
+++ app/src/main/java/com/example/jordan/icook/PreferencesActivity.java
@@ -1,23 +1,35 @@
 package com.example.jordan.icook;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
 public class PreferencesActivity extends AppCompatActivity {
+
+    private GestureDetectorCompat gestureObject; //for Gestre class
     DatabaseHelper myDb;
     int defaultAmount = 999;
     static int click = 0; //once you click, you can't click multiple times
                           // which adds a bunch to the pantry
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
         myDb = new DatabaseHelper(this);
 
-        Button normalButton = (Button)(findViewById(R.id.button));
+        //For Gestures
+        gestureObject = new GestureDetectorCompat(this, new LearnGesture());
+        //Class File
+
+        Button normalButton = findViewById(R.id.button);
         normalButton.setOnClickListener(new View.OnClickListener() {
             @Override
             //upon click, pantry will be filled and list of items will be shown
@@ -41,7 +53,7 @@ public class PreferencesActivity extends AppCompatActivity {
                 }
             }
         });
-        Button veganButton = (Button)(findViewById(R.id.button2));
+        Button veganButton = findViewById(R.id.button2);
         veganButton.setOnClickListener(new View.OnClickListener() {
             @Override
             //upon click, pantry will be filled and list of items will be shown
@@ -65,7 +77,7 @@ public class PreferencesActivity extends AppCompatActivity {
                 }
             }
         });
-        Button vegButton = (Button)(findViewById(R.id.button3));
+        Button vegButton = findViewById(R.id.button3);
         vegButton.setOnClickListener(new View.OnClickListener() {
             @Override
             //upon click, pantry will be filled and list of items will be shown
@@ -89,5 +101,46 @@ public class PreferencesActivity extends AppCompatActivity {
                 }
             }
         });
+
+        Button pantryInfo = findViewById(R.id.infoPantryLoadout);
+        pantryInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder infoDialog = new AlertDialog.Builder(PreferencesActivity.this);
+                infoDialog.setMessage("A default pantry loadout is to help create a pantry.\n\nPlease choose one, " +
+                        "the pre-made loadout wil be added to your pantry. \n\nNote: This is great for first time users.");
+                infoDialog.setCancelable(true);
+                infoDialog.setPositiveButton("OK", null);
+                infoDialog.setTitle("Pantry Loadouts");
+                infoDialog.show();
+            }
+        });
     }
+//FOR GESTURES, So Swipes Open New Activites. Shortcuts :)
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.gestureObject.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+    //For Gesture Object Class
+    class LearnGesture extends GestureDetector.SimpleOnGestureListener{
+        //Simple Gesture Listener for what we want to do, this opens the pantry
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY){
+        if(event2.getX() > event1.getX()){
+             //Here is the code for what you want the swipe to do for Left to Right
+            Intent openPantry = new Intent(PreferencesActivity.this, ListPantry.class);
+            finish(); //Ends current activities Actions
+            startActivity(openPantry);
+        }
+        else
+            if(event2.getX() < event1.getX()){
+                //Here is the code for what you want the swipe to do for Right to Left
+
+            }
+            return true;
+        }
+
+    }
+
 }
